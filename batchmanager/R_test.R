3e9d93588e2d878414d3098a0571e04b9937ada6
Dominik Mueller <domi89@pcpool203.ipsp.uni-hohenheim.de>
2017-01-23 17:27:23 +0100

    Modified R_test to write to stdout and stderr.

--- batchmanager/R_test.R
+++ batchmanager/R_test.R
@@ -2,10 +2,19 @@
 args = commandArgs(trailingOnly=TRUE)
 dput(args)
 
+print('This is some stdout.')
+
 time1 = Sys.time()
 while(Sys.time() - time1 < as.integer(args[1L])) {
   ## Sys.sleep(1)
 }
+
+print('This is more stdout.')
+
+stop('Here, an Error occurred.')
+
+print('This will not be received in stdout.')
+
 
 
 
